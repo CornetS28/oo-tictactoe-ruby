cc19fe2a6a11a623e54afdc3d23b44fe2eda57d1
EmirVatric <vatricemir@gmail.com>
2019-08-05 18:38:30 +0200

    class board

--- bin/main.rb
+++ bin/main.rb
@@ -1,26 +1,26 @@
-require_relative '../config/environment'
+require './lib/player.rb'
+require './lib/board.rb'
+class Main
 
-input = ""
-until input == "exit"
-  system "cls" or system "clear"
-  puts "Welcome to Samuel's & Emir's Tic-tac-toe Game"
-
-  puts "What Would You Like To Do:", "1. Player vs Player","2. Exit"
-
-  kind = gets.strip.to_i
-
-  case kind
-
-  when 1
-    system "cls" or system "clear"
-    game = Game.new
-    game.play
-  when 2
-    input = "exit"
+  def initialize
+    @player1 = Player.new
+    puts 'Please enter the name of the Player One'
+    @player1.name = gets.chop
+    @player2 = Player.new
+    puts 'Please enter the name of the Player Two'
+    @player2.name = gets.chomp
+    puts "#{@player1.get_name} please select sign X or O"
+    @player1.figure = gets.chomp.upcase
+    @player2.figure = @player1.figure == 'X' ? 'O' : 'X'
+    puts "#{@player1.get_name}'s figure is #{@player1.get_figure}, #{@player2.get_name}'s figure is #{@player2.get_figure}"
+    puts
+    puts "#{@player1.get_name} plays first, please select number on the board:"
+    puts
+    @board = Board.new
+    @board.display_board
+    
   end
-  break if input == "exit"
-  puts "\n","Do you want to re-play? (y/n)"
-  input = gets.strip.downcase
-  input = "exit" if input == "n" or input == "no"
+
 end
-puts "Good Bye!!"
+
+Main.new
